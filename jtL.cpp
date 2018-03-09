6d214df2530473420706d61a8329719bbcfdc0c8
Student <student@lab-315-018.cs.csubak.edu>
2018-03-09 15:02:15 -0800

    adding the 3rd time ..

---
+++ jtL.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+
+//Setup timers
+const double OOBILLION = 1.0 / 1e9;
+extern struct timespec timeStart, timeCurrent;
+extern double timeDiff(struct timespec *start, struct timespec *end);
+extern void timeCopy(struct timespec *dest, struct timespec *source);
+//--------------------------------------------------------------------
+
+
+int jt_lab7()
+{
+	return 0;
+}
+
+
+int divide( void ) 
+{
+	return 0;
+}
+
+int divide_opt ( void )
+{
+	return 0;
+} 
+
+
+
+
